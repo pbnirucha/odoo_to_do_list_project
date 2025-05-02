fe67f74680977a28f2de4e2818eb0e8d754aedb1
Nirucha Prasrisilsakul <88574898+pbnirucha@users.noreply.github.com>
2025-05-02 13:38:52 +0700

    add new file : __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Todo List',
+    'version': '1.0',
+    'summary': 'Todo List',
+    'description': """
+        Software Developer Excercise - Trinity Roots
+        Todo List
+    """,
+    'category': 'Productivity',
+    'author': 'Nirucha Prasrisilsakul',
+    'sequence': -1000,
+    'depends': [],
+    'data': [
+        'data/data_demo.xml',
+        'security/ir.model.access.csv',
+        'views/todo_list_view.xml',
+        'views/todo_tags_view.xml',
+        'views/menus.xml',
+        
+    ],
+    
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+
+}
